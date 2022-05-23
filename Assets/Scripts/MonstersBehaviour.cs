8ffef435dcefb9296691b99f6b51a5b4ce838a69
Goonie19 <antonio.espinosabarrios98@gmail.com>
2022-05-23 18:16:37 +0200

    Boss starting to work

--- Assets/Scripts/MonstersBehaviour.cs
+++ Assets/Scripts/MonstersBehaviour.cs
@@ -3,6 +3,7 @@ using Sirenix.OdinInspector;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem.Interactions;
 using UnityEngine.UI;
@@ -10,13 +11,22 @@ using UnityEngine.UI;
 public class MonstersBehaviour : MonoBehaviour
 {
     public Image HealthFillImage;
+    public Image Renderer;
+    public Image BossRenderer;
+
     public Transform DamageDisplaySpawn;
     public GameObject DamageInfoPrefab;
     [Title("Oleadas")]
     [SerializeField]
+    public AnimatorController EnemyAnimator;
     public List<MonsterWave> waves;
     public Button NextMonsterButton;
     public Button PreviousMonsterButton;
+
+    [Title("Boss Final")]
+    public Image FlashImage;
+    public AnimatorController BossAnimator;
+    public MonsterData BossData;
 
     #region MONSTER WAVE BEHAVIOUR
 
@@ -39,7 +49,7 @@ public class MonstersBehaviour : MonoBehaviour
 
     }
     [Serializable]
-    public struct MonsterData
+    public class MonsterData
     {
         public string Name;
         public Sprite sprite;
@@ -52,8 +62,10 @@ public class MonstersBehaviour : MonoBehaviour
 
     private float _life;
     private bool _dead = false;
-    private Image _renderer;
     private Animator _anim;
+
+    private bool _isOnBoss;
+    private float _bossActualLifePoints;
 
     private int _monstersLevels;
 
@@ -63,7 +75,7 @@ public class MonstersBehaviour : MonoBehaviour
     {
         _monstersLevels = 0;
         _life = waves[_monstersLevels].GetMonster().LifePoints;
-        _renderer = GetComponentInChildren<Image>();
+        _bossActualLifePoints = BossData.LifePoints;
         _anim = GetComponent<Animator>();
         NextMonsterButton.interactable = waves[_monstersLevels].Completed;
 
@@ -74,25 +86,50 @@ public class MonstersBehaviour : MonoBehaviour
     public void TakeDamage(float damage, bool animate = true, bool player = false)
     {
         
-        if(_life > 0)
+        if(!_isOnBoss)
         {
-            _life -= damage;
-
-            if(player)
+            if (_life > 0)
             {
-                GameObject display = Instantiate(DamageInfoPrefab, DamageDisplaySpawn);
-                display.GetComponent<DamageInfoText>().SetDamage(damage);
+                _life -= damage;
 
-                FMOD.Studio.EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(Placeholders.HIT_SFX_EVENT_PATH);
-                instance.start();
+                if (player)
+                {
+                    GameObject display = Instantiate(DamageInfoPrefab, DamageDisplaySpawn);
+                    display.GetComponent<DamageInfoText>().SetDamage(damage);
+
+                    FMOD.Studio.EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(Placeholders.HIT_SFX_EVENT_PATH);
+                    instance.start();
+                }
+
+                HealthFillImage.fillAmount = _life / (waves[_monstersLevels].GetMonster().LifePoints);
+
+                if (_life <= 0 && !_dead)
+                    Die();
+                else if (animate)
+                    _anim.SetTrigger("GetHit");
             }
+        } else
+        {
+            if (_bossActualLifePoints > 0)
+            {
+                _bossActualLifePoints -= damage;
 
-            HealthFillImage.fillAmount = _life / (waves[_monstersLevels].GetMonster().LifePoints);
+                if (player)
+                {
+                    GameObject display = Instantiate(DamageInfoPrefab, DamageDisplaySpawn);
+                    display.GetComponent<DamageInfoText>().SetDamage(damage);
 
-            if (_life <= 0 && !_dead)
-                Die();
-            else if(animate)
-                _anim.SetTrigger("GetHit");
+                    FMOD.Studio.EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(Placeholders.HIT_SFX_EVENT_PATH);
+                    instance.start();
+                }
+
+                HealthFillImage.fillAmount = _bossActualLifePoints / (BossData.LifePoints);
+
+                if (_life <= 0 && !_dead)
+                    BossDie();
+                else if (animate)
+                    _anim.SetTrigger("GetHit");
+            }
         }
 
         
@@ -121,12 +158,11 @@ public class MonstersBehaviour : MonoBehaviour
         Invoke("Spawn", 1f);
     }
 
-
     void Spawn()
     {
         _dead = false;        
 
-        _renderer.sprite = waves[_monstersLevels].GetMonster().sprite;
+        Renderer.sprite = waves[_monstersLevels].GetMonster().sprite;
         _life = waves[_monstersLevels].GetMonster().LifePoints;
         HealthFillImage.fillAmount = 1;
 
@@ -137,10 +173,59 @@ public class MonstersBehaviour : MonoBehaviour
         else
             PreviousMonsterButton.interactable = true;
 
-        _renderer.SetNativeSize();
+        Renderer.SetNativeSize();
         _anim.SetTrigger("Spawn");
     }
-    
+
+    #region BOSS
+
+    [ContextMenu("Spawn Boss")]
+    void BossSpawn()
+    {
+        _anim.runtimeAnimatorController = BossAnimator;
+        _isOnBoss = true;
+
+        Sequence sq = DOTween.Sequence();
+
+        sq.Append(FlashImage.DOFade(1, 1).SetEase(Ease.Linear));
+
+        sq.Play();
+
+        sq.OnComplete(() => { BossRenderer.gameObject.SetActive(true); Renderer.gameObject.SetActive(false); Deflash(); });
+
+
+    }
+
+    void Deflash()
+    {
+        Sequence sq = DOTween.Sequence();
+
+        sq.Append(FlashImage.DOFade(0, 1).SetEase(Ease.Linear));
+
+        sq.Play();
+    }
+
+    [ContextMenu("Boss Die")]
+    private void BossDie()
+    {
+        StartCoroutine(BossDieCoroutine());
+    }
+
+    IEnumerator BossDieCoroutine()
+    {
+        while (BossRenderer.material.GetFloat("_Fade") > 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            float g = BossRenderer.material.GetFloat("_Fade");
+            g -= 0.1f;
+            BossRenderer.material.SetFloat("_Fade", g);
+        }
+
+
+    }
+
+    #endregion
+
     public void ChangeMonster(bool add)
     {
         if (add)
@@ -175,8 +260,8 @@ public class MonstersBehaviour : MonoBehaviour
     public void changeRandomSprite()
     {
         
-        _renderer.sprite = waves[_monstersLevels].GetMonster().sprite;
-        _renderer.SetNativeSize();
+        Renderer.sprite = waves[_monstersLevels].GetMonster().sprite;
+        Renderer.SetNativeSize();
     }
 
 }
